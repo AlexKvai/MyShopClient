57b84acfc76334f2c32cb9c4c2ca323ca4384b6c
alex parfenov <alexp-2004@mail.ru>
2023-12-01 07:49:59 +0700

    Добавлен redux и нужные слайсы

---
+++ store/user/user.slice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { register } from './user.actions'
+import { IInitialState } from './user.interface'
+
+const initialState: IInitialState = {
+	user: localStorage.getItem('user')
+		? JSON.parse(localStorage.getItem('user') as string)
+		: null,
+	isLoading: false
+}
+
+export const userSlice = createSlice({
+	name: 'user',
+	initialState,
+	reducers: {},
+	extraReducers: builder => {
+		builder.addCase(register.pending, state => {
+			state.isLoading = true
+		}),
+			builder.addCase(register.fulfilled, (state, action) => {
+				state.isLoading = false
+				state.user = action.payload.user
+			}),
+			builder.addCase(register.rejected, state => {
+				state.isLoading = false
+			})
+	}
+})
