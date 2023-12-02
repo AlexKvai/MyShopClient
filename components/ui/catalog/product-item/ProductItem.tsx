9519273885f5d3fa332f2af8c5b5ec28096cfef9
alex parfenov <alexp-2004@mail.ru>
2023-12-02 21:02:07 +0700

    Добавлен ProductItem

---
+++ components/ui/catalog/product-item/ProductItem.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import { FC } from 'react'
+import { IProduct } from '../../../../types/product.interface'
+import AddToCartButton from './AddToCartButton'
+import FavoriteButton from './FavoriteButton'
+import ProductRating from './ProductRating'
+
+const ProductItem: FC<{ product: IProduct }> = ({ product }) => {
+	return (
+		<div>
+			<div>
+				<FavoriteButton productId={product.id} />
+				<AddToCartButton product={product} />
+				<Image
+					width={300}
+					height={300}
+					src={product.images[0]}
+					alt={product.name}
+				/>
+				<h3>{product.name}</h3>
+				<div>{product.category.name}</div>
+				<ProductRating product={product} />
+				<div>{product.price}</div>
+			</div>
+		</div>
+	)
+}
+
+export default ProductItem
