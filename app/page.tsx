c4da92ea19094f36ad97f61133ccdd627c732b2c
alex parfenov <alexp-2004@mail.ru>
2023-12-02 21:51:36 +0700

    Добавлена домашняя страница и данные для загрузки

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,32 @@
-import { NextPage } from 'next'
+'use client'
+import { useEffect, useState } from 'react'
 import Home from '../components/screens/home/Home'
+import { ProductService } from '../services/product/product.service'
+import { TypePaginationProducts } from '../types/product.interface'
 
-const HomePage: NextPage = () => {
-	return <Home />
+const HomePage: React.FC<TypePaginationProducts> = () => {
+	const [data, setData] = useState<TypePaginationProducts>({
+		products: [],
+		length: 0
+	})
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const { data } = await ProductService.getAll({
+					page: 1,
+					perPage: 4
+				})
+				setData(data)
+			} catch (error) {
+				console.error('Error fetching data:', error)
+			}
+		}
+
+		fetchData()
+	}, [])
+
+	return <Home products={data.products} length={data.length} />
 }
 
 export default HomePage
