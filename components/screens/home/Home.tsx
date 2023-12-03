63ae1f22f39ff64e9dd1dbd5a83f8da46febe3e7
alex parfenov <alexp-2004@mail.ru>
2023-12-03 12:53:22 +0700

    Добавлена кнопка выхода на главной странице

--- components/screens/home/Home.tsx
+++ components/screens/home/Home.tsx
@@ -1,12 +1,20 @@
 import { FC } from 'react'
+import { useActions } from '../../../hooks/useActions'
+import { useAuth } from '../../../hooks/useAuth'
 import { TypePaginationProducts } from '../../../types/product.interface'
 import Meta from '../../ui/Meta'
 import Catalog from '../../ui/catalog/Catalog'
+import Layout from '../../ui/layout/Layout'
 
 const Home: FC<TypePaginationProducts> = ({ products, length }) => {
+	const { user } = useAuth()
+	const { logout } = useActions()
 	return (
 		<Meta title='Home'>
-			<Catalog title='Freshed products' products={products || []} />
+			<Layout>
+				{!!user && <button onClick={() => logout()}>Logout</button>}
+				<Catalog title='Freshed products' products={products || []} />
+			</Layout>
 		</Meta>
 	)
 }
