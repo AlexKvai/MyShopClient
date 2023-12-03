4309a18680ee504847b5452226e0fd13de8be29b
alex parfenov <alexp-2004@mail.ru>
2023-12-03 11:57:38 +0700

    Изменён title в home

--- components/screens/home/Home.tsx
+++ components/screens/home/Home.tsx
@@ -1,15 +1,12 @@
 import { FC } from 'react'
 import { TypePaginationProducts } from '../../../types/product.interface'
-import Heading from '../../ui/Heading'
 import Meta from '../../ui/Meta'
 import Catalog from '../../ui/catalog/Catalog'
 
 const Home: FC<TypePaginationProducts> = ({ products, length }) => {
 	return (
 		<Meta title='Home'>
-			<Heading>Home</Heading>
-
-			<Catalog products={products || []} />
+			<Catalog title='Freshed products' products={products || []} />
 		</Meta>
 	)
 }
