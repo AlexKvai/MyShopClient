cb26363c8881e11201c6c6dee21c13596587aca7
alex parfenov <alexp-2004@mail.ru>
2023-12-05 15:23:28 +0700

    Добавлены Enum и TypeData + функция оплаты

--- services/order.service.ts
+++ services/order.service.ts
@@ -3,11 +3,35 @@ import { IOrder } from '../types/order.interface'
 
 const ORDERS = 'orders'
 
+enum EnumOrderStatus {
+	PENDING = 'PENDING',
+	PAYED = 'PAYED',
+	SHIPPED = 'SHIPPED',
+	DELIVERED = 'DELIVERED'
+}
+
+type TypeData = {
+	status?: EnumOrderStatus
+	items: {
+		quantity: number
+		price: number
+		productId: number
+	}[]
+}
+
 export const OrderService = {
 	async getAll() {
 		return instance<IOrder[]>({
 			url: ORDERS,
 			method: 'GET'
+		})
+	},
+
+	async place(data: TypeData) {
+		return instance<{ confirmation: { confirmation_url: string } }>({
+			url: ORDERS,
+			method: 'POST',
+			data
 		})
 	}
 }
