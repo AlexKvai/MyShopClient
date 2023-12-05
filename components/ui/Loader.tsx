3d68cf973503af1336847b37b4c20ea20991c5f4
alex parfenov <alexp-2004@mail.ru>
2023-12-05 09:46:11 +0700

    Добавлен новый Loader

--- components/ui/Loader.tsx
+++ components/ui/Loader.tsx
@@ -1,29 +1,18 @@
-const Loader = () => {
+import { Spinner } from '@chakra-ui/react'
+import { FC } from 'react'
+
+const Loader: FC = () => {
 	return (
-		<svg
-			width='50px'
-			height='50px'
-			viewBox='0 0 66 66'
-			xmlns='http://www.w3.org/2000/svg'
-			xmlnsXlink='http://www.w3.org/1999/xlink'
-			aria-hidden='true'
-			role='img'
-			preserveAspectRatio='xMidYMid meet'
-			fill='#000000'
-		>
-			<g transform='rotate(0)'>
-				<circle cx='33' cy='33' r='6' fill='#000000' fillOpacity='1'>
-					<animateTransform
-						attributeName='transform'
-						type='rotate'
-						from='0 33 33'
-						to='360 33 33'
-						dur='1s'
-						repeatCount='indefinite'
-					></animateTransform>
-				</circle>
-			</g>
-		</svg>
+		<Spinner
+			thickness='4px'
+			speed='1s'
+			emptyColor='gray.200'
+			color='green'
+			size='xl'
+			display='block'
+			className='mx-auto'
+			marginTop={20}
+		/>
 	)
 }
 
